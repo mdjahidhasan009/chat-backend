5db26aeaa2c4e798c1cb84eb4f5d9c05f579a283
mdjahidhasan009 <jahid60012@gmail.com>
2022-12-10 16:50:58 +0600

    setup auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { Routes } from '../utils/types';
+
+@Controller(Routes.AUTH)
+export class AuthController {
+
+}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { Services } from '../utils/types';
+
+@Module({
+  controllers: [AuthController],
+  providers: [
+    {
+      provide: Services.AUTH,
+      useClass: AuthService,
+    },
+  ],
+})
+export class AuthModule {}
